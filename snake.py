d6f8cdf0f4104d2ffa56ea08fd9d57c33cf5ff0f
abatima <renat.kulalov.work@gmail.com>
2026-01-04 00:41:56 +0100

    cleanup and added movement rules

--- snake.py
+++ snake.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-test = Turtle()
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snake_parts = []
         self.create()
+        self.snake_head = self.snake_parts[0]
 
     def create(self):
      for position in STARTING_POSITIONS:
@@ -21,18 +25,22 @@ class Snake:
             new_x = self.snake_parts[snake_part_number - 1].xcor()
             new_y = self.snake_parts[snake_part_number - 1].ycor()
             self.snake_parts[snake_part_number].goto(new_x, new_y)
-        self.snake_parts[0].fd(MOVE_DISTANCE)
+        self.snake_head.fd(MOVE_DISTANCE)
 
     def up(self):
-        self.snake_parts[0].setheading(90)
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
 
     def down(self):
-        self.snake_parts[0].setheading(270)
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
 
     def left(self):
-        self.snake_parts[0].setheading(180)
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
 
     def right(self):
-        self.snake_parts[0].setheading(0)
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
 
 
